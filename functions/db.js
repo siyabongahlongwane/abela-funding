623e24e63cebf336aec4ea5ad2c7aac20cdd3cea
Siyabonga Hlongwane <hlongwanesiyabonga6@gmail.com>
2024-08-11 21:46:59 +0200

    chore: removed logging

--- functions/db.js
+++ functions/db.js
@@ -6,7 +6,6 @@ const DB_CONNECTION = {
     db: process.env.DB_CONNECTION_DB
 }
 const CONNECTION_URI = `mongodb+srv://${DB_CONNECTION.username}:${DB_CONNECTION.password}@${DB_CONNECTION.host}/${DB_CONNECTION.db}?retryWrites=true&w=majority`
-console.log(CONNECTION_URI);
 mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) console.log(err);
     else console.log('DB connection established!')
